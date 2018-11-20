413013d73121645cae0e180ea43dfa8b268e1348
jforcada <jforcada@conwet.com>
2018-11-20 16:48:49 +0100

    Create Maven project

---
+++ src/main/java/PROF_2018_EX1/Forcada/App.java
@@ -0,0 +1,13 @@
+package PROF_2018_EX1.Forcada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
